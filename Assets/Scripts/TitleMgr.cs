ed8a48f5609cd6634e13e1e8e8546d1654c93d53
YoukeKz <peace_syk-kz1@yahoo.co.jp>
2019-09-18 14:49:24 +0900

    FirstCommit

---
+++ Assets/Scripts/TitleMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMgr : MonoBehaviour
+{
+    void OnGUI()
+    {
+		Util.SetFontSize( 32 );
+		Util.GUILabel();
+    }
+}

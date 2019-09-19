ba5f422668b1c075bab56ee44df8bd913669f89a
YoukeKz <peace_syk-kz1@yahoo.co.jp>
2019-09-19 23:51:47 +0900

    SceneMainの追加

--- Assets/Scripts/TitleMgr.cs
+++ Assets/Scripts/TitleMgr.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleMgr : MonoBehaviour
 {
     void OnGUI()
     {
 		Util.SetFontSize( 32 );
-		Util.GUILabel();
+
+        float x = 128;
+        float y = 32;
+        float h = ( Screen.width - x );
+        float w = ( Screen.height - y );
+		Util.GUILabel( x, y, w, h, "Title" );
+		
+        x += 60;
+        if( GUI.Button( new Rect( x, y, w, h ), "START" ) ){
+			SceneManager.CreateScene( "SceneMain" );
+		}
     }
 }
